dde59f886c2cafbf6dd5849c76d3376ce1f03b90
Jun Byeon <qusdudwns0162@gmail.com>
2023-08-02 20:55:22 +0900

    Add files via upload

--- Backend/exception/src/main/java/com/example/exception/controller/UserApiController.java
+++ Backend/exception/src/main/java/com/example/exception/controller/UserApiController.java
@@ -2,6 +2,7 @@ package com.example.exception.controller;
 
 import com.example.exception.model.Api;
 import com.example.exception.model.UserResponse;
+import org.springframework.boot.context.config.ConfigDataNotFoundException;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -17,6 +18,9 @@ public class UserApiController {
             UserResponse.builder().id("2").age(10).name("James").build());
     @GetMapping("/id/{userId}")
     public Api<UserResponse> getUser(@PathVariable String userId){
+        if(true){
+            throw new RuntimeException("message");
+        }
         var user = userList.stream().filter(it -> it.getId().equals(userId)).findFirst().get();
         Api<UserResponse> response = Api.<UserResponse>builder().resultCode(String.valueOf(HttpStatus.OK.value())).resultMessage(HttpStatus.OK.name()).data(user).build();
         return response;
--- Backend/exception/src/main/java/com/example/exception/exception/RestApiExceptionHandler.java
+++ Backend/exception/src/main/java/com/example/exception/exception/RestApiExceptionHandler.java
@@ -4,6 +4,7 @@ import com.example.exception.controller.RestApiBController;
 import com.example.exception.controller.RestApiController;
 import com.example.exception.model.Api;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.core.annotation.Order;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
@@ -14,12 +15,9 @@ import java.util.NoSuchElementException;
 
 @Slf4j
 @RestControllerAdvice(basePackageClasses = { RestApiController.class, RestApiBController.class })
+@Order(1)
 public class RestApiExceptionHandler {
-    @ExceptionHandler(value = {Exception.class})
-    public ResponseEntity exception(Exception e){
-        log.error("RestApiExceptionHandler", e);
-        return ResponseEntity.status(200).build();
-    }
+
     @ExceptionHandler(value = {IndexOutOfBoundsException.class})
     public ResponseEntity outOfBound(IndexOutOfBoundsException e){
         log.error("RestApiExceptionHandler kkk", e);
@@ -27,8 +25,9 @@ public class RestApiExceptionHandler {
     }
 
     @ExceptionHandler(value = { NoSuchElementException.class })
-    public Api noSuchElement(NoSuchElementException e){
+    public ResponseEntity<Api> noSuchElement(NoSuchElementException e){
         log.error("", e);
-        return Api.builder().resultCode(String.valueOf(HttpStatus)).resultMessage().build();
+        var response = Api.builder().resultCode(String.valueOf(HttpStatus.NOT_FOUND.value())).resultMessage(HttpStatus.NOT_FOUND.getReasonPhrase()).build();
+        return ResponseEntity.status(HttpStatus.NOT_FOUND).body(response);
     }
 }
